4efb8f2e13b766ecdc6477fec5b96df817409eed
Nick Weiland <nickweiland@gmail.com>
2016-03-22 18:12:17 -0600

    update_authorization_for_borrowers

--- app/controllers/borrower/base_controller.rb
+++ app/controllers/borrower/base_controller.rb
@@ -2,6 +2,6 @@ class Admin::BaseController < ApplicationController
   before_action :require_borrower
 
   def require_borrower
-    render file: "/public/404" unless current_admin?
+    render file: "/public/404" unless current_borrower?
   end
 end
