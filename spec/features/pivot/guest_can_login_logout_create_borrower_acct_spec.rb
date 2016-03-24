728725ed17de89647a4bce9b23757cc782b85da8
ZooBot Mars <sasa345@yahoo.com>
2016-03-23 20:19:33 -0600

    lender and borrower can login and logout

---
+++ spec/features/pivot/guest_can_login_logout_create_borrower_acct_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature "GuestCanCreateLenderAccountAndLogout" do
+  scenario "guest can register with lender account and logout" do
+    create_user
+    logout_user
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Sad to see you go Mister. Come back again soon.")
+  end
+
+  scenario "lender can login" do
+    create_user
+    logout_user
+    expect(current_path).to eq(root_path)
+    login_user
+    expect(page).to have_content("Hey Mister, welcome to Lending Owl.")
+  end
+end
--- spec/features/pivot/registered_borrower_can_login_and_logouts_spec.rb
+++ spec/features/pivot/registered_borrower_can_login_and_logouts_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "RegisteredBorrowerCanLoginAndLogouts" do
+RSpec.feature "RegisteredBorrowerCanLoginAndLogout" do
 
   scenario "registered borrower can log in" do
     create(:lender_role)
@@ -20,19 +20,23 @@ RSpec.feature "RegisteredBorrowerCanLoginAndLogouts" do
     expect(current_path).to eq(borrower_user_path(user))
   end
 
-  scenario "guest can register with lender account and logout" do
-    create_user
+  scenario "registered borrower can log out" do
+    create(:lender_role)
+    create(:borrower_role)
+    user = create(:user)
+    borrower = create(:borrower)
+    borrower.user_id = user.id
+    user.roles << Role.find_by(name:"borrower")
+    visit root_path
+    click_on "login"
+    expect(current_path).to eq(login_path)
+    within("div#login-form") do
+      fill_in "email", with: "jane@gmail.com"
+      fill_in "password", with: "password"
+      click_on "login"
+    end
     logout_user
     expect(current_path).to eq(root_path)
-    expect(page).to have_content("Sad to see you go Mister. Come back again soon.")
-  end
-
-  scenario "lender can login" do
-    create_user
-    logout_user
-    expect(current_path).to eq(root_path)
-    login_user
-    expect(page).to have_content("Hey Mister, welcome to Lending Owl.")
   end
 
 end
