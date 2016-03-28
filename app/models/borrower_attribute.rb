ed97c99e9396e6d3af2236c65cc19720972bbcd4
Nick Weiland <nickweiland@gmail.com>
2016-03-28 11:46:23 -0600

    Add BorrowerAttributes table to manage borrower application

---
+++ app/models/borrower_attribute.rb
@@ -0,0 +1,2 @@
+class BorrowerAttribute < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328015216) do
+ActiveRecord::Schema.define(version: 20160328174021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "borrower_attributes", force: :cascade do |t|
+    t.string   "category"
+    t.string   "label"
+    t.integer  "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "borrowers", force: :cascade do |t|
     t.integer  "annual_income"
