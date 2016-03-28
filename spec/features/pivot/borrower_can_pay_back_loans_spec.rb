ba49327ba09b33e7877659b5cc88fba4eae3f3fa
ZooBot Mars <sasa345@yahoo.com>
2016-03-27 18:01:51 -0600

    borrower can pay toward a loan

---
+++ spec/features/pivot/borrower_can_pay_back_loans_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+RSpec.feature "BorrowerPaysLoans", type: :feature do
+  scenario "they have option to pay back a portion of loan" do
+    create(:lender_role)
+    create(:borrower_role)
+    borrower_user = create(:user, fullname: "borrower jones", email: "borrow@email.com")
+    lender_user = create(:user, fullname: "lender smith", email: "lender@email.com")
+    borrower_user.roles << Role.find_by(name:"borrower")
+    borrower = create(:borrower)
+    borrower.update(user_id: borrower_user.id)
+    category1 = create(:category)
+    category2 = create(:category, name: "space!")
+    country1 = create(:country)
+    country2 = create(:country, name: "nicaragua")
+    project_active1 = create(:project, borrower_id: borrower.id, category_id: category1.id, country_id: country1.id)
+    project_active2 = create(:project, name: "rockets", borrower_id: borrower.id, category_id: category2.id, country_id: country2.id)
+    order = lender_user.orders.create
+    loan1 = order.loans.create(quantity: 25, project_id: project_active1.id)
+    loan2 = order.loans.create(quantity: 30, project_id: project_active1.id)
+    loan3 = order.loans.create(quantity: 35, project_id: project_active2.id)
+    # borrower_user.borrower.loans => "all loans"
+
+    visit root_path
+    click_on "login"
+    within("div#login-form") do
+      fill_in "email", with: borrower_user.email
+      fill_in "password", with: borrower_user.password
+      click_on "login"
+    end
+
+    click_on "active loans"
+
+    within "tr##{loan1.id}-loan" do
+      click_on "pay loan"
+      expect(current_path).to eq(borrower_user_loan_path(borrower_user, loan1))
+    end
+
+    expect(page).to have_content("loan toward project #{project_active1.name}")
+
+    select "5", from: "amount"
+    click_on "pay now"
+
+    within "tr##{loan1.id}-loan" do
+      expect(page).to have_content("20")
+    end
+
+  end
+
+end
