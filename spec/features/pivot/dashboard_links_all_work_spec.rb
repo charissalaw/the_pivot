7cdf8fe40926fbfca004668e39be38c88c933aab
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-03-29 14:41:25 -0600

    Home Page links route to expected pages

---
+++ spec/features/pivot/dashboard_links_all_work_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "dashboard links work" do
+  scenario "each homepage link can be clicked and leads to valid page" do
+    create(:category, name: "agriculture")
+    create(:category, name: "food")
+    create(:category, name: "education")
+
+    visit "/"
+    click_on "become a lender"
+    expect(current_path).to eq "/lend"
+
+    visit "/"
+    click_on "become a borrower"
+    expect(current_path).to eq "/borrower/users/new"
+
+    visit "/"
+    click_on "lend for agriculture"
+    expect(current_path).to eq "/categories/agriculture"
+
+    visit "/"
+    click_on "lend for education"
+    expect(current_path).to eq "/categories/education"
+
+    visit "/"
+    click_on "lend for food"
+    expect(current_path).to eq "/categories/food"
+  end
+end
