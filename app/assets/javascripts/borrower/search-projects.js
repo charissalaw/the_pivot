9f91b88b478852b4948f54b40aae44cca6ad3747
ZooBot Mars <sasa345@yahoo.com>
2016-03-29 19:53:52 -0600

    javascript search is not case sensitive and numbers are filtered by sequence

--- app/assets/javascripts/borrower/search-projects.js
+++ app/assets/javascripts/borrower/search-projects.js
@@ -1,12 +1,12 @@
 function searchProjects(){
   $('#project-search').keyup(function(event){
-    var amountSearch = $(this).val()
+    var amountSearch = ($(this).val()).toLowerCase()
+
     var allProjects = $('.squeek')
-    console.log(allProjects);
 
     allProjects.removeClass('invisible')
     var hideIdeas = allProjects.filter(function(){
-      var projectTable = $(this).find('.project-name').text()
+      var projectTable = ($(this).find('.project-name').text()).toLowerCase()
       return !(projectTable.includes(amountSearch))
     })
     hideIdeas.addClass('invisible');
