3ae084cb7b1e6c79f3dc3d1205cfeea4aa48a86a
Nick Weiland <nickweiland@gmail.com>
2016-03-30 17:22:22 -0600

    add search to project indices

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,8 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by_slug(params[:slug])
   end
+
+  def search
+    redirect_to category_path(params[:category_search])
+  end
 end
