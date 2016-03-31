8602eeb9dfd7c8b11b602de27001a8b59d9a9093
ZooBot Mars <sasa345@yahoo.com>
2016-03-30 21:14:38 -0600

    testing repayment controller

--- spec/features/pivot/borrower_can_pay_back_loans_spec.rb
+++ spec/features/pivot/borrower_can_pay_back_loans_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "BorrowerPaysLoans", type: :feature do
-  scenario "they have option to pay back a portion of loan" do
+  scenario "they have option to pay back a portion of funding" do
     create(:lender_role)
     create(:borrower_role)
     borrower_user = create(:user, fullname: "borrower jones", email: "borrow@email.com")
@@ -13,9 +13,10 @@ RSpec.feature "BorrowerPaysLoans", type: :feature do
     category2 = create(:category, name: "space!")
     country1 = create(:country)
     country2 = create(:country, name: "nicaragua")
-    project_active1 = create(:project, borrower_id: borrower.id, category_id: category1.id, country_id: country1.id)
+    project_active1 = create(:project, goal: 50000, borrower_id: borrower.id, category_id: category1.id, country_id: country1.id)
     order = lender_user.orders.create
     order.loans.create(quantity: 2500, project_id: project_active1.id)
+    order.loans.create(quantity: 4000, project_id: project_active1.id)
 
     visit root_path
     click_on "login"
@@ -28,19 +29,20 @@ RSpec.feature "BorrowerPaysLoans", type: :feature do
     click_on "active loans"
     expect(current_path).to eq(borrower_user_loans_path(borrower_user))
 
-    within"div##{project_active1.id}-project" do
-      click_on "pay back"
-      expect(page).to have_content("repayment toward project #{project_active1.name}")
-      expect(page).to have_content("Remaining amount: $90.0")
-      select "10", from: "amount_paid"
+    within"##{project_active1.id}-project-detail" do
       click_on "pay back"
     end
 
-    expect(current_path).to eq(borrower_user_loans_path(borrower_user))
+    expect(page).to have_content("repayment toward project #{project_active1.name}")
+    expect(page).to have_content("Remaining amount: $65.0")
+    select "30", from: "amount_paid"
+
+    project_active1.repayments.create(project_id: project_active1.id, amount_paid: 30)
+    visit borrower_user_loans_path(borrower_user)
     expect(page).to_not have_content("No loans yet!")
 
-    within "div##{project_active1.id}-project" do
-      expect(page).to have_content("Left to Pay: $80.0")
+    within "##{project_active1.id}-project-detail" do
+      expect(page).to have_content("Left to Pay: $35.0")
     end
   end
 end
