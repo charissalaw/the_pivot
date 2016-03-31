395ad463676e7c08b5729961c882a4e39f0a141e
Nick Weiland <nickweiland@gmail.com>
2016-03-31 13:18:39 -0600

    add borrow permissions

--- app/services/permission.rb
+++ app/services/permission.rb
@@ -74,6 +74,7 @@ private
     return true if controller == 'projects' && action.in?(%w(index show))
     return true if controller == 'orders' && action.in?(%w(new create index show thanks))
     return true if controller == 'carts' && action.in?(%w(show))
+    return true if controller == 'borrower/users' && action.in?(%w(new create))
     return true if controller == 'cart_projects' && action.in?(%w(create destroy update))
     return true if controller == 'borrowers' && action.in?(%w(new create))
     return true if controller == "categories" && action.in?(%w(show search))
